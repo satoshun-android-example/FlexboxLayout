275de4c86f317bf88988190e3d2abbdbe6e7fda1
SatoShun <shun.sato1@gmail.com>
2020-01-22 08:40:38 +0000

    hoge

--- app/src/main/java/com/github/satoshun/example/main/maxline/MaxLineFragment.kt
+++ app/src/main/java/com/github/satoshun/example/main/maxline/MaxLineFragment.kt
@@ -5,7 +5,6 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.github.satoshun.example.R
 import com.github.satoshun.example.databinding.FlexboxMaxLineBinding
-import com.github.satoshun.example.databinding.FlexboxPerfFragBinding
 import com.google.android.material.chip.Chip
 
 class MaxLineFragment : Fragment(R.layout.flexbox_max_line) {

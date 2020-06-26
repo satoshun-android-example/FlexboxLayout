1c6417fe4c3d0b152ea3a3b1b1871c97dc169f40
SatoShun <shun.sato1@gmail.com>
2020-06-26 05:44:23 +0000

    test

--- app/src/main/java/com/github/satoshun/example/AppActivity.kt
+++ app/src/main/java/com/github/satoshun/example/AppActivity.kt
@@ -7,6 +7,8 @@ import com.github.satoshun.example.databinding.AppActBinding
 class AppActivity : AppCompatActivity() {
   private lateinit var binding: AppActBinding
 
+
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = AppActBinding.inflate(layoutInflater)
